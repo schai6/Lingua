b340b427891c9d009e7577a431f75f3dbcf7e9a4
Sam Chai <sam.chai0501@gmail.com>
2018-02-21 19:35:16 -0500

    add default values for picking language/quest

--- client/components/PickLanguage.js
+++ client/components/PickLanguage.js
@@ -7,7 +7,8 @@ export class PickLanguage extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedQuestId: -1
+      //default to Spanish
+      selectedLanguage: 1
     }
   }
 
--- client/components/PickQuest.js
+++ client/components/PickQuest.js
@@ -7,7 +7,8 @@ export class PickQuest extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedQuestId: -1
+      //default to first quest
+      selectedQuest: 1
     }
   }
 
